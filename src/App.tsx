896238cd4a6c094b332162d25dea50a4b769ee1e
irfanbughlani <vidly9@gmail.com>
2024-11-12 03:42:05 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,6 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import CompanyPage from './pages/CompanyPage';
 import { companies } from './data/companies';
-import OcrCaptchaSolver from './components/OcrCaptchaSolver';
-
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>CAPTCHA Solver Demo</h1>
-      <OcrCaptchaSolver />
-    </div>
-  );
-}
-
-export default App;
 
 function App() {
   return (
