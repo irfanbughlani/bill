6c6e85376d0d7a3feb940150b59f12e329646e54
irfanbughlani <vidly9@gmail.com>
2024-11-12 04:33:47 +0500

    Create Disclaimer.tsx

---
+++ src/pages/Disclaimer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Disclaimer: React.FC = () => {
+  return (
+    <div>
+      <h1>Disclaimer</h1>
+      <p>This is the Disclaimer page content.</p>
+    </div>
+  );
+};
+
+export default Disclaimer;
