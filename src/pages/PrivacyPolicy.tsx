78809db1dbe3cfa337b41b68b389db05b78174ff
irfanbughlani <vidly9@gmail.com>
2024-11-12 04:33:32 +0500

    Create PrivacyPolicy.tsx

---
+++ src/pages/PrivacyPolicy.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrivacyPolicy: React.FC = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>This is the Privacy Policy page content.</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
