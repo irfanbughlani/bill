9a8e146ad30e25b056244a5a7967f52f89d37c60
irfanbughlani <vidly9@gmail.com>
2024-11-12 04:45:54 +0500

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,11 +7,11 @@ const About: React.FC = () => {
         <h1 className="text-4xl font-bold text-center text-blue-600 mb-6">About Us</h1>
         <p className="text-gray-700 leading-relaxed text-lg mb-4">
           Welcome to our platform! We aim to provide seamless access to essential information
-          regarding various utilities in Pakistan, enabling users to check and monitor their bills
-          effortlessly. Our platform serves as a bridge between users and utility providers.
+          regarding various utility bills in Pakistan, enabling users to check and monitor their bills
+          effortlessly. Our platform serves as a bridge between users and utility bill providers online.
         </p>
         <p className="text-gray-700 leading-relaxed text-lg mb-4">
-          Our mission is to empower users with easy-to-access resources and improve transparency
+          Our mission is to empower users with easy-to-access to onlien bill checking resources and improve transparency
           for various services. We hope you find our platform helpful and easy to use.
         </p>
       </div>
