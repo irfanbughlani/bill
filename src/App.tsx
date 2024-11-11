41f94f73a2cfbb1e379d3739c802f36c9be7cb3d
irfanbughlani <vidly9@gmail.com>
2024-11-11 22:09:19 +0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import CompanyPage from './pages/CompanyPage';
+import { companies } from './data/companies';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col bg-gradient-to-br from-blue-50 to-indigo-50">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {companies.map((company) => (
+              <Route
+                key={company.id}
+                path={`/${company.id.toLowerCase()}`}
+                element={<CompanyPage company={company} />}
+              />
+            ))}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
